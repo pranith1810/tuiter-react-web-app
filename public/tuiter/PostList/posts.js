1dd07335ca95e8c22d4966afdf058d832ed0e2de
Pranith Rao <pranithraonayeneni@gmail.com>
2022-10-16 23:05:49 -0400

    fixing images not loading

--- public/tuiter/PostList/posts.js
+++ public/tuiter/PostList/posts.js
@@ -1,12 +1,12 @@
 export default [
   {
-    avatar: "/public/images/naruto.jpeg",
+    avatar: "/images/naruto.jpeg",
     handle: "Kakashi Katsuya",
     id: "KK",
     date: "Oct 13",
     content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
     pretium hendrerit porta.`,
-    banner: "/public/images/naruto-big1.jpeg",
+    banner: "/images/naruto-big1.jpeg",
     bannerContentTitle: `Lorem ipsum dolor sit amet, consectetur adipiscing
     elit`,
     bannerContent: `Duis sed sodales lectus. In hac habitasse platea
@@ -17,14 +17,14 @@ export default [
     hearts: 37,
   },
   {
-    avatar: "/public/images/boruto.jpeg",
+    avatar: "/images/boruto.jpeg",
     handle: "King Bong",
     id: "KB",
     date: "Dec 10",
     content: ` Sed ac mauris et lectus blandit accumsan at at quam. Curabitur mauris quam,
     ullamcorper at semper in, tristique in tortor. Vivamus ipsum metus, dictum ut venenatis id,
     rutrum eu justo. In eu mi nec urna vestibulum pellentesque.`,
-    banner: "/public/images/naruto-big2.png",
+    banner: "/images/naruto-big2.png",
     bannerContentTitle: `Lorem ipsum dolor sit amet, consectetur adipiscing
     elit`,
     bannerContent: `Duis sed sodales lectus. In hac habitasse platea
